e5ac37288a99d29ba1848c1bd0aab3b351271319
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-09-20 20:00:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -185,7 +185,7 @@ def long_name_steals_a_ton?
   end
   if steals == steals_long_name
 end
-
+end
 
 #Write a method that returns true if the player with the longest name had the most steals. Call the method long_name_steals_a_ton?.
 
